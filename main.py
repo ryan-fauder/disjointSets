f1af7a5632053572339bec166eebd3e5ec5cc1b2
Ryan Auder <ryan.fauder@gmail.com>
2021-08-26 16:13:47 -0300

    f: union and find were added

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ class DisjointSet:
     self.parents = [x for x in range(size)]
   def __repr__(self):
     return "Size: {0}\nParents:\n{1}\nRanks\n{2}\n".format(self.size, " ".join(map(str, self.parents)), " ".join(map(str, self.ranks)))
+  def findRepr(self, child):
+    if(child != self.parents[child]):
+      return findRepr(self, self.parents[child])
+    return self.parents[child]
+  def union(self, child1, child2):
+    repr1, repr2 = self.findRepr(child1), self.findRepr(child2)
+    if(repr1 != repr2):
+      self.parents[repr1] = repr2
 
 if(__name__ == "__main__"):
   size = int(input())
+  if size < 4:
+    size = 4
   tree = DisjointSet(size)
+  print(tree)
+  tree.union(1, 2)
+  tree.union(2, 3)
   print(tree)
